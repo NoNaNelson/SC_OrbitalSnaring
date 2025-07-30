694d65b01258abae57da7a6bfe57dc2b39d91564
nonanelson <nilshorling@gmail.com>
2025-07-30 21:39:06 +0200

    finished build models script and system model

--- build_data.py
+++ build_data.py
@@ -3,26 +3,30 @@ from src.models.planet import Planet
 from src.models.system import System
 
 # iterate through files in the "data/" directory
-for file in os.listdir("src/data/"):
-    l=[]
-    data = None
-    if file.endswith(".json") and file.startswith("raw_"):
-        with open(os.path.join("src/data/", file), "r") as f:
-            data = json.load(f)
-    if not data:
-        continue
-    print(data)
+def main():
+    for file in os.listdir("src/data/"):
+        l=[]
+        data = None
+        if file.endswith(".json") and file.startswith("raw_"):
+            with open(os.path.join("src/data/", file), "r") as f:
+                data = json.load(f)
+        if not data:
+            continue
+        print(data)
 
-    for obj in data["structured"][0]["objects"]:
-        if obj["type"] == "Planet" or obj["type"] == "Moon":
-            print(obj)
-            l.append(Planet(**obj).model_dump())
-            for moon_obj in obj["objects"]:
-                if moon_obj["type"] == "Moon" or moon_obj["type"] == "Planet":
-                    l.append(Planet(**moon_obj).model_dump())
-                for sub_obj in obj["objects"]:
-                    if sub_obj["type"] == "Moon" or sub_obj["type"] == "Planet":
-                        l.append(Planet(**sub_obj).model_dump())
-                        # write to file
-    with open(os.path.join("src/data/", file.replace("raw_", '')), "w") as f:
-        json.dump(l, f, indent=4)
+        for obj in data["structured"][0]["objects"]:
+            if obj["type"] == "Planet" or obj["type"] == "Moon":
+                print(obj)
+                l.append(Planet(**obj).model_dump())
+                for moon_obj in obj["objects"]:
+                    if moon_obj["type"] == "Moon" or moon_obj["type"] == "Planet":
+                        l.append(Planet(**moon_obj).model_dump())
+                    for sub_obj in obj["objects"]:
+                        if sub_obj["type"] == "Moon" or sub_obj["type"] == "Planet":
+                            l.append(Planet(**sub_obj).model_dump())
+                            # write to file
+        with open(os.path.join("src/data/", file.replace("raw_", '')), "w") as f:
+            json.dump(l, f, indent=4)
+
+if __name__ == "__main__":
+    main()
--- src/models/system.py
+++ src/models/system.py
@@ -1,35 +1,22 @@
-from src.models.vector import Vector3
-from src.models.planet import Planet
-from enum import Enum
+import json
 from typing import List
+from pydantic import BaseModel, Field
 
-class System(Enum):
+from src.models.planet import Planet
 
-    @classmethod
-    def name(cls) -> str:
-        """Return the name of the system."""
-        return cls.__name__
-    @classmethod
-    def __iter__(cls):
-        return iter(cls.__members__.values())
+class System(BaseModel):
+    name: str
     
-    @classmethod
-    def default_planet(cls) -> Planet:
-        """Return the first planet in the system, if available."""
-        return cls.planets()[0]
+    planets: List[Planet]
+
+
+
+        
     
-    @classmethod
-    def planets(cls) -> List[Planet]:
-        return [member.value for member in cls.__members__.values() if isinstance(member.value, Planet)]
     
+
 
 if  __name__ == "__main__":
-    # Example usage
-    sun = System(data={
-        "name": "Sun",
-        "x": 0,
-        "y": 0,
-        "z": 0,
-        "rotation_speed": 1,
-        "type": "Star"
-    })
+    with open("../data/stanton.json", "r") as f:
+            data = json.load(f)
+    print(System(name="Stanton", planets=data).model_dump())
