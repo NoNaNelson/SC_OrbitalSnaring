8e2a6618e9d3ce028cda286878262817e19438dc
nonanelson <nilshorling@gmail.com>
2025-07-30 22:37:03 +0200

    fixed issue with duplicate data

--- build_data.py
+++ build_data.py
@@ -12,19 +12,15 @@ def main():
                 data = json.load(f)
         if not data:
             continue
-        print(data)
 
         for obj in data["structured"][0]["objects"]:
             if obj["type"] == "Planet" or obj["type"] == "Moon":
-                print(obj)
+                # print(obj)
                 l.append(Planet(**obj).model_dump())
                 for moon_obj in obj["objects"]:
                     if moon_obj["type"] == "Moon" or moon_obj["type"] == "Planet":
                         l.append(Planet(**moon_obj).model_dump())
-                    for sub_obj in obj["objects"]:
-                        if sub_obj["type"] == "Moon" or sub_obj["type"] == "Planet":
-                            l.append(Planet(**sub_obj).model_dump())
-                            # write to file
+                    
         with open(os.path.join("src/data/", file.replace("raw_", '')), "w") as f:
             json.dump(l, f, indent=4)
 
